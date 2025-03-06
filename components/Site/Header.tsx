b00c074c30236d9202ac19883ff51c29a4f49da8
miracleonyenma <miracleficient@gmail.com>
2025-03-06 09:39:51 +0100

    feat: enhance site header styling and structure, remove unused authentication store

--- components/Site/Header.tsx
+++ components/Site/Header.tsx
@@ -1,7 +1,5 @@
 // ./components/Site/Header.tsx
-
 "use client";
-
 import { auth } from "@/firebase";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -18,8 +16,7 @@ const SiteHeader = () => {
     const provider = new GoogleAuthProvider();
     try {
       await signInWithPopup(auth, provider).then(async (result) => {
-        const savedUser = await createUser(result);
-        console.log({ savedUser });
+        await createUser(result);
       });
     } catch (error) {
       console.log(error);
@@ -27,28 +24,28 @@ const SiteHeader = () => {
   };
 
   return (
-    <header className="site-header sticky top-0 z-50 flex items-center justify-between bg-white px-4 py-3 shadow-sm shadow-gray-200 dark:bg-gray-900 dark:shadow-gray-700">
-      <div className="wrapper w-full flex items-center gap-4 justify-between mx-auto max-w-5xl">
+    <header className="site-header">
+      <div className="wrapper">
         <Link href="/">
           <h1 className="font-bold">Tasks App</h1>
         </Link>
 
         {user ? (
-          <div className="flex items-center gap-2">
-            <p className="text-sm flex items-center gap-2 text-gray-400 dark:text-gray-500">
+          <div className="user-btn ">
+            <figure className="img-cont">
               <Image
                 src={user.photoURL || "/images/avatar.png"}
                 alt="avatar"
                 width={32}
                 height={32}
-                className="rounded-full shadow border border-gray-200 dark:border-gray-700"
+                className=""
               />
-            </p>
+            </figure>
             <button
               className="btn primary sm"
               onClick={() => {
                 auth.signOut();
-                router.push("/");
+                router.refresh();
               }}
             >
               Sign Out
