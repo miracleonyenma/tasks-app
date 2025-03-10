a72a4c208523037ed8204faf7a86ae543a026ae3
Miracle Onyenma <miracleficient@gmail.com>
2025-03-10 10:25:07 +0000

    feat: improve error handling in organization creation and user addition forms

--- components/Org/Form.tsx
+++ components/Org/Form.tsx
@@ -55,7 +55,9 @@ const CreateOrgForm: React.FC<{ onClose: () => void }> = ({ onClose }) => {
         cb(data.org?.id);
         return "Organization created successfully";
       },
-      error: "Error creating organization",
+      error: (error) => {
+        return error.message;
+      },
       finally: () => {
         setLoading(false);
         formik.setSubmitting(false);
@@ -73,7 +75,9 @@ const CreateOrgForm: React.FC<{ onClose: () => void }> = ({ onClose }) => {
         console.log({ data });
         return "User added to organization successfully";
       },
-      error: "Error adding user to organization",
+      error: (error) => {
+        return error.message;
+      },
       finally: () => {
         setLoading(false);
         formik.setSubmitting(false);
